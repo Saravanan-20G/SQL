f2328815d9764cf162cdf27ee5fa4a5cc5dfea39
Saravanan G <159242456+Saravanan-20G@users.noreply.github.com>
2024-08-14 16:35:45 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,5 @@
+select * from suppliers
+
+insert into suppliers(supplier_id,supplier_name,contact_name,phone,email,address) values (1,'san','saran','65978','fbgg@gmail.com','0,sdkoforo')
+
+select * from purchases
